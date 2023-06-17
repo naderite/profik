219102b04e369c008d72f4c9a6b65a49e75da981
naderite <64203130+naderite@users.noreply.github.com>
2023-06-17 01:07:39 +0100

    refactored forms and backend logic

--- backend/profikapp/backend_logic.py
+++ backend/profikapp/backend_logic.py
@@ -1,73 +1,98 @@
-from .models import Exercise, Correction
+from .models import Exercise, Correction,Question
 import random
 
+def find_exercise(exercise_length, exercise_course_part, exercise_goal, exercise_difficulty):
+    """
+    Finds an exercise based on the provided parameters.
 
-def find_exercise(exercice_longueur, exercice_partie_cours, exercice_but, exercice_difficulte):
-    exercises = list(Exercise.objects.filter(
-        longueur=exercice_longueur,
-        course_part_id=exercice_partie_cours,
-        but=exercice_but,
-        difficulte=exercice_difficulte
-    ))
+    Returns a randomly selected exercise matching the parameters, or None if no exercise is found.
+    """
+    exercises = Exercise.objects.filter(
+        length=exercise_length,
+        course_part=exercise_course_part,
+        goal=exercise_goal,
+        difficulty=exercise_difficulty
+    )
     return random.choice(exercises) if exercises else None
 
 
-def find_correction(correction_theoreme, correction_cours, correction_nombre_de_methode, correction_commentaire, question_id):
-    corrections = list(Correction.objects.filter(theoreme=correction_theoreme,
-                                                cours=correction_cours,
-                                                nombre_de_methode=correction_nombre_de_methode,
-                                                commentaires=correction_commentaire,
-                                                question_id=question_id))
+def find_correction(correction_theorem, correction_course, correction_has_methods, correction_comments, question):
+    """
+    Finds a correction based on the provided parameters.
+
+    Returns a randomly selected correction matching the parameters, or None if no correction is found.
+    """
+    corrections = Correction.objects.filter(
+        has_theorem=correction_theorem,
+        course=correction_course,
+        has_methods=correction_has_methods,
+        comments=correction_comments,
+        question=question
+    )
     return random.choice(corrections) if corrections else None
 
 
+
 def search_exercise(request):
-    exercice_longueur = request.GET.get('longueur')
-    exercice_cours = request.GET.get('cours')
-    exercice_partie_cours = request.GET.get('partie_cours')
-    exercice_but = request.GET.get('but')
-    exercice_difficulte = request.GET.get('difficulte')
+    """
+    Searches for an exercise based on the query parameters in the request.
+
+    Returns a randomly selected exercise matching the query parameters, or None if no exercise is found.
+    """
+    exercise_length = request.GET.get('length')
+    exercise_course_part = request.GET.get('course_part')
+    exercise_goal = request.GET.get('goal')
+    exercise_difficulty = request.GET.get('difficulty')
     return find_exercise(
-        exercice_longueur,
-        exercice_cours,
-        exercice_partie_cours,
-        exercice_but,
-        exercice_difficulte,
+        exercise_length,
+        exercise_course_part,
+        exercise_goal,
+        exercise_difficulty,
     )
 
 
-def search_correction(ex_id, request):
-    correction_theoreme = bool(request.GET.get('theoreme'))
-    correction_cours = request.GET.get('cours')
-    correction_nombre_de_methode = bool(request.GET.get('nombre-de-methode'))
-    correction_commentaire = request.GET.get('commentaire')
+def search_correction(question_id, request):
+    """
+    Searches for a correction based on the query parameters in the request.
+
+    Returns a randomly selected correction matching the query parameters, or None if no correction is found.
+    """
+    correction_theorem = bool(request.GET.get('theorem'))
+    correction_course = request.GET.get('course')
+    correction_has_methods = bool(request.GET.get('has_methods'))
+    correction_comments = request.GET.get('comments')
+    question = Question.objects.get(id=question_id)
     return find_correction(
-        correction_theoreme,
-        correction_cours,
-        correction_nombre_de_methode,
-        correction_commentaire,
-        ex_id,
+        correction_theorem,
+        correction_course,
+        correction_has_methods,
+        correction_comments,
+        question,
     )
 
 
 def extract_exercise_data_from_form(form):
-    exercise_niveau = form.cleaned_data['niveau']
-    exercise_course_part_id = form.cleaned_data['course_part_id']
-    exercise_longueur = form.cleaned_data['longueur']
-    exercise_but = form.cleaned_data['but']
-    exercise_difficulte = form.cleaned_data['difficulte']
-    return (
-        exercise_niveau,
-        exercise_course_part_id,
-        exercise_longueur,
-        exercise_but,
-        exercise_difficulte,
-    )
+    """
+    Extracts exercise data from a form.
+
+    Returns a tuple containing the extracted exercise data.
+    """
+    exercise_level = form.cleaned_data['level']
+    exercise_course_part = form.cleaned_data['course_part']
+    exercise_length = form.cleaned_data['length']
+    exercise_goal = form.cleaned_data['goal']
+    exercise_difficulty = form.cleaned_data['difficulty']
+    return exercise_level, exercise_course_part, exercise_length, exercise_goal, exercise_difficulty
 
 
 def fill_exercise_data(exercise, exercise_data):
-    exercise.niveau = exercise_data[0]
-    exercise.course_part_id = exercise_data[1]
-    exercise.longueur = exercise_data[2]
-    exercise.but = exercise_data[3]
-    exercise.difficulte = exercise_data[4]
+    """
+    Fills exercise data into the provided exercise instance.
+
+    Modifies the exercise instance with the provided exercise data.
+    """
+    exercise.level = exercise_data[0]
+    exercise.course_part = exercise_data[1]
+    exercise.length = exercise_data[2]
+    exercise.goal = exercise_data[3]
+    exercise.difficulty = exercise_data[4]
--- backend/profikapp/forms.py
+++ backend/profikapp/forms.py
@@ -1,51 +1,68 @@
 from django import forms
-from .models import Exercise, Course, CoursePart
-LONGUEUR_CHOICES = [
-    (0, 'court'),
-    (1, 'long'),
-    (2, 'probleme'),
-]
-DIFFICULTE_CHOICES = [
-    (0, 'facile'),
-    (1, 'moyen'),
-    (2, 'deficile'),
-]
-COMMENTAIRES_CHOICES = [
-    (0, 'minimum'),
-    (1, 'moyen'),
-    (2, 'trés explicatif'),
-]
-THEOREME_CHOICES = [
-        (True, 'inclus'),
-        (False, 'non')
-    ]
-NOMBRE_DE_CHOICES = [(True, 'Seule'), (False, 'Multiple')] 
+from .models import Course, CoursePart
 
 
 class ExerciseForm(forms.Form):
-    niveau = forms.CharField(label='Exercise Niveau')
-    cours = forms.ModelChoiceField(queryset=Course.objects.all(), label='Exercise Cours')
-    course_part_id = forms.ModelChoiceField(queryset=CoursePart.objects.all(), label='Exercise partie de cours')
-    longueur = forms.ChoiceField(choices=LONGUEUR_CHOICES, label='Exercise longueur')
-    but = forms.CharField(label='Exercise but')
-    difficulte = forms.ChoiceField(choices=DIFFICULTE_CHOICES, label='Exercise difficulte')
+    """
+    Form for creating an exercise.
+    """
+    LENGTH_CHOICES = [
+    (0, 'court'),  # Short length
+    (1, 'long'),  # Long length
+    (2, 'probleme'),  # Problem length
+    ]
+
+    DIFFICULTY_CHOICES = [
+        (0, 'facile'),  # Easy difficulty
+        (1, 'moyen'),  # Medium difficulty
+        (2, 'difficile'),  # Difficult difficulty
+    ]
+    level = forms.CharField(label='Niveau de l\'exercice')  # Exercise level
+    course = forms.ModelChoiceField(queryset=Course.objects.all(), label='Cours de l\'exercice')  # Exercise course
+    course_part = forms.ModelChoiceField(queryset=CoursePart.objects.all(), label='Partie de cours de l\'exercice')  # Exercise course part
+    length = forms.ChoiceField(choices=LENGTH_CHOICES, label='Longueur de l\'exercice')  # Exercise length
+    goal = forms.CharField(label='But de l\'exercice')  # Exercise goal
+    difficulty = forms.ChoiceField(choices=DIFFICULTY_CHOICES, label='Difficulté de l\'exercice')  # Exercise difficulty
+
 
 class QuestionForm(forms.Form):
-    order = forms.CharField(label="l'ordre de la question:")
-    text = forms.CharField(widget=forms.Textarea,label='text')
+    """
+    Form for creating a question.
+    """
+    order = forms.CharField(label='Ordre de la question')  # Question order
+    text = forms.CharField(widget=forms.Textarea, label='Texte de la question')  # Question text
+
 
 class CorrectionForm(forms.Form):
-    text = forms.CharField(widget=forms.Textarea,label='text:')
-    theoreme = forms.ChoiceField(
-        choices=THEOREME_CHOICES,
-        label='Theoreme',
+    """
+    Form for creating a correction.
+    """
+    COMMENT_CHOICES = [
+    (0, 'minimum'),  # Minimum comments
+    (1, 'moyen'),  # Medium comments
+    (2, 'très explicatif'),  # Highly explanatory comments
+    ]
+
+    THEOREM_CHOICES = [
+        (True, 'inclus'),  # Included theorem
+        (False, 'non inclus'),  # Not included theorem
+    ]
+
+    METHODS_CHOICES = [
+        (True, 'Unique'),  # Single number of methods
+        (False, 'Multiple'),  # Multiple number of methods
+    ]
+    text = forms.CharField(widget=forms.Textarea, label='Texte de la correction')  # Correction text
+    has_theorem = forms.ChoiceField(
+        choices=THEOREM_CHOICES,
+        label='Théorème',
         initial=True,
-        widget=forms.HiddenInput(attrs={'value': True}),
-    )
-    nombre_de_methode = forms.ChoiceField(
-        choices=NOMBRE_DE_CHOICES,
+        widget=forms.HiddenInput,
+    )  # Theorem inclusion
+    has_method = forms.ChoiceField(
+        choices=METHODS_CHOICES,
         initial=True,
-        label='Nombre de Methode',
-        widget=forms.HiddenInput(attrs={'value': True}),
-    )    
-    commentaires = forms.IntegerField(label='Commentaires', widget=forms.HiddenInput())
+        label='Nombre de méthodes',
+        widget=forms.HiddenInput,
+    )  # Number of methods
+    comments = forms.IntegerField(label='Commentaires', widget=forms.HiddenInput)  # Comment count
